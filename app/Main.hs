d1c2ca10a4025a434eba1e5c57c496240ebc65a3
Davor Lucic <davor.lucic@ost.ch>
2024-11-28 22:12:47 +0100

    Tui dev gigafix

--- app/Main.hs
+++ app/Main.hs
@@ -77,46 +77,6 @@ appEvent :: BrickEvent Name e -> EventM Name AppState ()
 appEvent (VtyEvent ev) = do
   currentState <- get
   case ev of
-    V.EvKey (V.KChar 'i') [] -> do
-      torrentInfo <- liftIO loadTorrentInfo
-      modify $ \s -> s {appContent = ["Torrent Info: " ++ torrentInfo]}
-    V.EvKey (V.KChar 'p') [] -> do
-      let filePath = "./sample.torrent"
-      fileContent <- liftIO $ LB.readFile filePath
-      let decoded = decodeBencodedValue (B.concat $ LB.toChunks fileContent)
-      let json = decodedToDictionary decoded
-      let query = makeQuery json
-      request <- liftIO $ parseRequest query
-      response <- liftIO $ httpLBS request
-      let peersBytes = decodedToDictionary (decodeBencodedValue $ LB.toStrict $ getResponseBody response) ! B.pack "peers"
-      let peersList = peersToAddressList $ decodedToByteString peersBytes
-      let formattedPeers = zipWith (\i peer -> "Peer " ++ show i ++ ": " ++ B.unpack peer) [2 ..] peersList
-      modify $ \s -> s {appContent = formattedPeers}
-    V.EvKey (V.KChar 'd') [] -> do
-      let filePath = "./sample.torrent"
-      fileContent <- liftIO $ LB.readFile filePath
-      let decoded = decodeBencodedValue (B.concat $ LB.toChunks fileContent)
-      let json = decodedToDictionary decoded
-      let info = decodedToDictionary (json ! B.pack "info")
-      let infoHash = calculateInfoHash (sortInfo info) ""
-      let tracker_url = B.unpack (decodedToByteString (json ! B.pack "announce"))
-      let query = makeQuery json
-      request <- liftIO $ parseRequest query
-      response <- liftIO $ httpLBS request
-      let peersBytes = decodedToDictionary (decodeBencodedValue $ LB.toStrict $ getResponseBody response) ! B.pack "peers"
-      let peersList = peersToAddressList $ decodedToByteString peersBytes
-      -- Assuming we always connect to the first peer
-      let peerAddress = head peersList
-      let fileLength = read (show $ info ! B.pack "length") :: Int
-      handle <- liftIO $ getSocketHandle (B.takeWhile (/= ':') peerAddress) (B.drop 2 $ B.dropWhile (/= ':') peerAddress) infoHash
-      liftIO $ waitForBitfield handle
-      liftIO $ sendInterested handle
-      liftIO $ waitForUnchoke handle
-      let pieceLength = read (show $ info ! B.pack "piece length") :: Int
-      let pieceIndex = 1 -- Assuming we download piece 0 for now
-      let outputPath = "./downloaded_piece" -- Assuming a fixed output path
-      liftIO $ getPiece handle pieceIndex pieceLength outputPath fileLength
-      modify $ \s -> s {appContent = ["Downloaded piece " ++ show pieceIndex ++ " to " ++ outputPath]}
     V.EvKey (V.KChar 'q') [] -> do
       BR.halt
     _ -> do
@@ -209,100 +169,7 @@ byob =
 ui :: Widget ()
 ui = C.vBox [box3, byob]
 
-loadTorrentInfo :: IO String
-loadTorrentInfo = do
-  let filePath = "./sample.torrent"
-  fileContent <- LB.readFile filePath
-  let decoded = decodeBencodedValue (B.concat $ LB.toChunks fileContent)
-  let json = decodedToDictionary decoded
-  let info = decodedToDictionary (json M.! B.pack "info")
-  let trackerUrl = B.unpack $ decodedToByteString (json M.! B.pack "announce")
-  let pieceLength = show (info M.! B.pack "piece length")
-  let fileLength = show (info M.! B.pack "length")
-  return $
-    unlines
-      [ "Tracker URL: " ++ trackerUrl,
-        "Piece Length: " ++ pieceLength,
-        "File Length: " ++ fileLength
-      ]
-
 main :: IO ()
 main = do
   finalState <- BR.defaultMain theApp initialState
   putStrLn $ "You chose: " <> show (D.dialogSelection (appDialog finalState))
-
--- do
--- args <- getArgs
--- when (length args < 3) $ do
---   putStrLn "Usage: your_bittorrent.sh <command> <args>"
---   exitWith (ExitFailure 2)
--- case args of
---   ["decode", encodedValue] -> do
---     let decodedValue = decodeBencodedValue (B.pack encodedValue)
---     print decodedValue
---   ["info", filePath] -> do
---     fileContent <- LB.readFile filePath
---     let json = decodedToDictionary $ decodeBencodedValue (B.concat $ LB.toChunks fileContent)
---     let info = decodedToDictionary (json ! B.pack "info")
---     putStrLn $ "Tracker URL: " ++ B.unpack (decodedToByteString (json ! B.pack "announce"))
---     putStrLn $ "Length: " ++ show (info ! B.pack "length")
---     putStrLn $ "Info Hash: " ++ B.unpack (calculateInfoHash (sortInfo info) "hex")
---     putStrLn $ "Piece Length: " ++ show (info ! B.pack "piece length")
---     putStrLn "Piece Hashes:"
---     let pieces = getPieceHashes $ decodedToByteString (info ! B.pack "pieces")
---     mapM_ (putStrLn . B.unpack . B17.encode) pieces
---   ["peers", filePath] -> do
---     let filePath = args !! 2
---     fileContent <- LB.readFile filePath
---     let json = decodedToDictionary $ decodeBencodedValue (B.concat $ LB.toChunks fileContent)
---     let query = makeQuery json
---     request <- parseRequest query
---     response <- httpLBS request
---     let peersBytes = decodedToDictionary (decodeBencodedValue $ LB.toStrict $ getResponseBody response) ! B.pack "peers"
---     let peersList = peersToAddressList $ decodedToByteString peersBytes
---     mapM_ (putStrLn . B.unpack) peersList
---   ["handshake", filePath, peerAddress] -> do
---     let peerIP = B.takeWhile (/= ':') $ B.pack peerAddress
---     let peerPort = B.drop 2 $ B.dropWhile (/= ':') $ B.pack peerAddress
---     fileContent <- LB.readFile filePath
---     let json = decodedToDictionary $ decodeBencodedValue (B.concat $ LB.toChunks fileContent)
---     let info = decodedToDictionary (json ! B.pack "info")
---     let infoHash = calculateInfoHash (sortInfo info) ""
---     handle <- getSocketHandle peerIP peerPort infoHash
---     response <- B.hGet handle 69
---     hClose handle
---     let peerId = B.drop 49 response
---     putStrLn $ "Peer ID: " ++ B.unpack (B17.encode peerId)
---   ["download_piece", "-o", outputPath, filePath, pieceIndex] -> do
---     fileContent <- LB.readFile filePath
---     let json = decodedToDictionary $ decodeBencodedValue (B.concat $ LB.toChunks fileContent)
---     let info = decodedToDictionary (json ! B.pack "info")
---     let infoHash = calculateInfoHash (sortInfo info) ""
---     let tracker_url = B.unpack (decodedToByteString (json ! B.pack "announce"))
---     let query = makeQuery json
---     request <- parseRequest query
---     response <- httpLBS request
---     let peersBytes = decodedToDictionary (decodeBencodedValue $ LB.toStrict $ getResponseBody response) ! B.pack "peers"
---     let peersList = peersToAddressList $ decodedToByteString peersBytes
---     let peerAddress = peersList !! 1
---     let fileLength = read (show $ info ! B.pack "length") :: Int
---     handle <- getSocketHandle (B.takeWhile (/= ':') peerAddress) (B.drop 2 $ B.dropWhile (/= ':') peerAddress) infoHash
-
---     waitForBitfield handle
---     -- putStrLn "Bitfield received!"
-
---     sendInterested handle
---     -- putStrLn "Interested sent!"
-
---     waitForUnchoke handle
-
---     let pieceLength = read (show $ info ! B.pack "piece length") :: Int
---     print $ "pieceLength" <> show pieceLength
---     print $ "fileLength" <> show fileLength
---     getPiece handle (read pieceIndex) pieceLength outputPath fileLength
-
---     putStrLn "Piece received!"
-
---     hClose handle
---   -- putStrLn "Piece downloaded!"
---   _ -> putStrLn $ "Unknown command/sequence: " ++ intercalate " " args
